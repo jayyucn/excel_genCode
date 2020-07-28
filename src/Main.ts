96c1e9e7a507bd950599730d9fe2c098949e2a75
jayyu <jayyu@123u.com>
2020-07-28 19:30:24 +0800

    ss

--- src/Main.ts
+++ src/Main.ts
@@ -1,7 +1,13 @@
 import ExcelReader from './reader/ExcelReader';
+import FS from 'fs';
 export default  class Main {
-    static start() {
-        ExcelReader.readExcel('./test/Menu.xlsx');
+    static start(configPath: string) {
+        ExcelReader.readExportExcel(configPath + 'ExportSetting.xlsx');
+        let fileList = FS.readdirSync(configPath + "config");
+        for(let file of fileList) {
+            
+            ExcelReader.readExcel('./test/Menu.xlsx');
+        }
     }
 }
 
